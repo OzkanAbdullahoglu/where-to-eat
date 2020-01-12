2f66eab16c24056612bf436474bc6aae29c62fc6
Özkan Abdullahoğlu <ozkanabdullahoglu@Ozkan-Mac-mini.local>
2020-01-12 23:04:42 +0300

    feat: Redux store

---
+++ src/store.js
@@ -0,0 +1,25 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
+import rootReducer from './reducers';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+
+const middlewares = [thunk];
+const persistConfig = {
+  key: 'root',
+  storage,
+  timeout: 0,
+  stateReconciler: autoMergeLevel2,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(
+  persistedReducer,
+  applyMiddleware(...middlewares)
+);
+
+const persistor = persistStore(store);
+
+export { store, persistor };
