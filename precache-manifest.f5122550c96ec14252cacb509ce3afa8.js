73a8415c76f3eba3f40bd804fe2e858cbe247fd7
Özkan Abdullahoğlu <ozkanabdullahoglu@Ozkan-Mac-mini.local>
2020-01-13 03:01:22 +0300

    Updates

---
+++ precache-manifest.f5122550c96ec14252cacb509ce3afa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf38481dfddfc40f2bcf32c76ef01613",
+    "url": "/where-to-eat/index.html"
+  },
+  {
+    "revision": "1a6416794e8608c911fb",
+    "url": "/where-to-eat/static/css/main.54dd3fbc.chunk.css"
+  },
+  {
+    "revision": "86d8c08f097659f0b5d8",
+    "url": "/where-to-eat/static/js/2.67ebe0a1.chunk.js"
+  },
+  {
+    "revision": "4b3d0b7ba3e309b48f461b9c97c8af98",
+    "url": "/where-to-eat/static/js/2.67ebe0a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a6416794e8608c911fb",
+    "url": "/where-to-eat/static/js/main.d55e7e53.chunk.js"
+  },
+  {
+    "revision": "3007945205a4b8ebb5dc",
+    "url": "/where-to-eat/static/js/runtime-main.ceccf651.js"
+  }
+]);
