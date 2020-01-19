0c1fbd54db695fb07f30e281c77214dd4d1a0ec0
OzkanAbdullahoglu <ozkanabdullahoglu@gmail.com>
2020-01-19 15:17:24 +0300

    feat: selectors added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 import { combineReducers } from 'redux';
 import mapReducer, {
   selectors as mapSelectors,
@@ -16,6 +17,14 @@ export const getFetchedData = (store) =>
   mapSelectors.getFetchedData(store.map);
 export const getLeftPanelStatus = (store) =>
   mapSelectors.getLeftPanelStatus(store.map);
+export const getSearchBoxStatus = (store) =>
+  mapSelectors.getSearchBoxStatus(store.map);
+export const getIsMainToggleButtonStatus = (store) =>
+  mapSelectors.getIsMainToggleButtonStatus(store.map);
+export const getIsAlertModalVisible = (store) =>
+  mapSelectors.getIsAlertModalVisible(store.map);
+export const getFetchStatus = (store) =>
+  mapSelectors.getFetchStatus(store.map);
 
 export { mapActions };
 
