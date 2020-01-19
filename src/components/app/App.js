b3b15046606a52d8257bf7aa6a4828151af55865
OzkanAbdullahoglu <ozkanabdullahoglu@gmail.com>
2020-01-19 15:16:11 +0300

    chore: folder structure changed

---
+++ src/components/app/App.js
@@ -0,0 +1,21 @@
+/* eslint-disable linebreak-style */
+import React from 'react';
+import { ApolloProvider } from 'react-apollo';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from '../../store';
+import Client from '../../Client';
+import Map from '../main/Map';
+import './App.css';
+
+const App = () => (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <ApolloProvider client={Client}>
+        <Map />
+      </ApolloProvider>
+    </PersistGate>
+  </Provider>
+);
+
+export default App;
