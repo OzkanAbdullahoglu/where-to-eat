f5077e5294447f624f26d94e30d2f45076280e28
OzkanAbdullahoglu <ozkanabdullahoglu@gmail.com>
2020-01-27 04:06:39 +0300

    test: refactoring test

--- src/components/main/Map/Map.test.js
+++ src/components/main/Map/Map.test.js
@@ -1,39 +1,57 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
 import { shallow } from 'enzyme';
-
+import toJson from 'enzyme-to-json';
 import { storeFactory } from '../../../utils/testUtils';
 import Map from './Map';
 
 describe('Map', () => {
-  const store = storeFactory();
-  const map = shallow(<Map store={store} />).dive().dive();
+  const setup = (state = {}) => {
+    const store = storeFactory(state);
+    const map = shallow(<Map store={store} />).dive().dive();
+    return map;
+  }
+  const mapWrapper = setup();
   it('renders map component correctly', () => {
-    expect(map).toMatchSnapshot();
+    
+    expect(toJson(mapWrapper)).toMatchSnapshot();
   });
   it('contains a Left Panel Component', () => {
-    expect(map.find('Connect(LeftPanel)').exists()).toBe(true);
+    expect(mapWrapper.find('Connect(BareLeftPanel)').exists()).toBe(true);
   });
   it('contains a Google Map Component', () => {
-    expect(map.find('Connect(withProps(lifecycle(withScriptjs(withGoogleMap(Component)))))').exists()).toBe(true);
+    expect(mapWrapper.find('Connect(withProps(lifecycle(withScriptjs(withGoogleMap(Component)))))').exists()).toBe(true);
   });
   it('contains a Search Box Component', () => {
-    expect(map.find('Connect(SearchMap)').exists()).toBe(true);
+    expect(mapWrapper.find('Connect(SearchMap)').exists()).toBe(true);
   });
   it('has an initial center with a lat 37.09024 and a lng -95.712891, and zoom 12', () => {
     const lat = 37.09024;
     const lng = -95.712891;
-    expect(map.state()).toEqual({ center: { lat, lng }, zoom: 12 });
+    expect(mapWrapper.state()).toEqual({ center: { lat, lng }, zoom: 12 });
   });
   describe('updating coordinates and zoom', () => {
     const newLat = 25.25;
     const newLng = 50.50;
     beforeEach(() => {
-      map.instance().markerLocationUpdate(newLat, newLng);
+      mapWrapper.instance().markerLocationUpdate(newLat, newLng);
     });
     it('updates coordinates and zoom', () => {
-      expect(map.state()).toEqual({ center: { lat: newLat, lng: newLng }, zoom: 15 });
+      expect(mapWrapper.state()).toEqual({ center: { lat: newLat, lng: newLng }, zoom: 15 });
+    });
+
+    it('triggers zoomToUser function to get the user location when mounted', () => {
+      const mapPropertyMock = jest.fn();
+      mapWrapper.instance().zoomToUserLocation = mapPropertyMock;
+      mapWrapper.instance().componentDidMount();
+      expect(mapPropertyMock.mock.calls.length).toBe(1);
+    });
+  });
+  describe('redux properties', () => {
+    it('has access to `isFetchedData` state', () => {
+      const isFetchedData = '';
+      const fetchedDataProp = mapWrapper.instance().props.store.getState().map.fetchedData;
+      expect(fetchedDataProp).toEqual(isFetchedData);
     });
   });
 });
-
