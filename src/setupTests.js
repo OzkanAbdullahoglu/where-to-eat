9540512bbc73863eaa53575c8d547b87c281dc6d
OzkanAbdullahoglu <ozkanabdullahoglu@gmail.com>
2020-01-27 03:59:02 +0300

    chore: setupTest additional config

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,11 +1,41 @@
 /* eslint-disable linebreak-style */
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
-
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter(), disableLifecycleMethods: true });
+
+window.google = {
+  maps: {
+    Marker: class { },
+    Map: class { setTilt() { } fitBounds() { } },
+    LatLngBounds: class { },
+    event: { addDomListener: () => {} },
+    places: {
+      Autocomplete: class { addListener() {} },
+      AutocompleteService: class { },
+      PlacesServiceStatus: {
+        INVALID_REQUEST: 'INVALID_REQUEST',
+        NOT_FOUND: 'NOT_FOUND',
+        OK: 'OK',
+        OVER_QUERY_LIMIT: 'OVER_QUERY_LIMIT',
+        REQUEST_DENIED: 'REQUEST_DENIED',
+        UNKNOWN_ERROR: 'UNKNOWN_ERROR',
+        ZERO_RESULTS: 'ZERO_RESULTS',
+      },
+      PlacesAutocomplete: {
+        INVALID_REQUEST: 'INVALID_REQUEST',
+        NOT_FOUND: 'NOT_FOUND',
+        OK: 'OK',
+        OVER_QUERY_LIMIT: 'OVER_QUERY_LIMIT',
+        REQUEST_DENIED: 'REQUEST_DENIED',
+        UNKNOWN_ERROR: 'UNKNOWN_ERROR',
+        ZERO_RESULTS: 'ZERO_RESULTS',
+      },
+    },
+
+    MarkerClusterer: class { },
+    Geocoder: class { },
+  },
+};
+
